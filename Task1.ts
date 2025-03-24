69d22839601fca3ef9a41e78b906569fb4604882
Hawraa <hawraalasad@yahoo.com>
2025-03-25 02:03:17 +0300

    task

---
+++ Task1.ts
@@ -0,0 +1,9 @@
+// Step 1. Declare Variables
+// 1.1. Create a constant named requiredWheels of type number aand set it's value to 4.
+
+// 1.2. Create a changeable variable named installedWheels of type number and set it's value to
+
+// Step 2. Write a condition
+// 2.1. Check if installedWheels equals requiredWheels:
+// If true, log: "Ready to go!".
+// If false, log: "Missing wheels"
